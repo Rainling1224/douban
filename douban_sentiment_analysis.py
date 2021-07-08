8e1692e17a6782a2602a580f28111b467f0c6fa3
Rainling1224 <87138856+Rainling1224@users.noreply.github.com>
2021-07-08 23:46:29 +0800

    Add files via upload

---
+++ douban_sentiment_analysis.py
@@ -0,0 +1,45 @@
+
+
+"""
+Created on Wed May  5 20:12:48 2021
+
+@author: 13397
+"""
+
+#encoding='GBK'
+
+import pandas as pd
+from snownlp import SnowNLP
+from snownlp import sentiment
+#import matplotlib.pyplot as plt
+
+#读取取的csv文件,标题在第3列,序号为2
+df=pd.read_csv('douban_movie.csv', header=None,usecols=[2])
+
+#将 dataframe转换为1ist
+contents=df.values.tolist()
+#数据长度
+print(len(contents))
+#定义空列表存储情感分值
+score=[]
+
+
+for content in contents:
+    #print(content)
+    #a=SnowNLP(content[0])
+    #print(a.words)
+    try:
+        s=SnowNLP(content[0])
+        #情绪判断，返回值为正面情绪的概率，越接近1表示正面情绪，越接近0表示负面情绪
+        score.append(s.sentiments)
+        #分词
+        print(s.words)
+    except:
+         print("something is wrong")
+         score.append (0.5)
+         
+#显示情感得分长度,与数据长度比较
+print(len(score))
+#存储
+data2=pd.DataFrame(score)
+data2.to_csv('sentiment.csv', header=False, index=False,mode='a+')
